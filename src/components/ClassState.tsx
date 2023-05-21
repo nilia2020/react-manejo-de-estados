2bd234405f1aa1cf7f8ced81f6bb9c63174f26f9
jorge niglia <jorge.niglia@gmail.com>
2023-05-21 15:23:32 -0300

    hasta estados semideclarativos

--- src/components/ClassState.tsx
+++ src/components/ClassState.tsx
@@ -6,6 +6,7 @@ type MyProps = {
 type MyState = {
   error: boolean;
   loading: boolean;
+  value: string;
 };
 
 import { Loading } from "./Loading";
@@ -14,32 +15,30 @@ class ClassState extends React.Component<MyProps, MyState> {
   state: MyState = {
     error: false,
     loading: false,
+    value: "",
   };
-  /* UNSAFE_componentWillMount(): void {
-    console.log("componentWillMount");
-  } */
-  /* componentDidMount(): void {
-    console.log("componentDidMount");
-  } */
   componentDidUpdate(prevProps: MyProps, prevState: MyState): void {
-    console.log("componentDidUpadate");
-    const { loading } = this.state;
+    const { loading, value } = this.state;
     if (!!loading) {
       setTimeout(() => {
         console.log("haciendo la validación");
-        this.setState({ loading: false });
+        if (value !== process.env.NEXT_PUBLIC_SECURITY_CODE) {
+          this.setState({ error: true, loading: false });
+        } else {
+          this.setState({ loading: false, error: false });
+        }
         console.log("finalizando la validación");
-      }, 3000);
+      }, 1500);
     }
   }
   render() {
     const { name } = this.props;
-    const { error, loading } = this.state;
+    const { error, loading, value } = this.state;
     return (
       <section className="flex flex-col items-center w-screen h-96 justify-evenly">
         <h2 className="text-3xl font-bold ">Eliminar {name}</h2>
         <p className="text-2xl ">Por favor, escribe el código de seguridad</p>
-        {error && (
+        {error && !loading && (
           <p className="text-xl font-medium text-red-600">
             error: el código es incorrecto
           </p>
@@ -49,6 +48,11 @@ class ClassState extends React.Component<MyProps, MyState> {
           <input
             placeholder="Código de seguridad"
             className="p-2 mx-4 text-xl text-center border-2 rounded-md"
+            value={value}
+            onChange={(e) => {
+              e.preventDefault();
+              this.setState({ value: e.target.value });
+            }}
           />
           <button
             className="p-2 text-xl font-semibold bg-green-200 border-2 border-green-700 rounded-md shadow-lg cursor-pointer text-slate-950 hover:bg-green-800 hover:text-slate-100"
--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -1,46 +1,147 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 
 const UseState = (props: { name: string }) => {
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [state, setState] = React.useState({
+    value: "",
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+  });
   const { name } = props;
+
+  const onConfirm = () => {
+    setState({
+      ...state,
+      error: false,
+      loading: false,
+      confirmed: true,
+    });
+  };
+  const onError = () => {
+    setState({
+      ...state,
+      error: true,
+      loading: false,
+    });
+  };
+  const onChangeInput = (newValue: string) => {
+    setState({
+      ...state,
+      value: newValue,
+    });
+  };
+
+  const onCheck = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+  };
+
+  const onDelete = () => {
+    setState({
+      ...state,
+      deleted: true,
+    });
+  };
+
+  const onReset = () => {
+    setState({
+      ...state,
+      confirmed: false,
+      deleted: false,
+      value: "",
+    });
+  };
+
   React.useEffect(() => {
-    console.log("empezando el efecto");
-    if (!!loading) {
+    if (!!state.loading) {
       setTimeout(() => {
-        console.log("haciendo la validación");
-        setLoading(false);
-        console.log("finalizando la validación");
-      }, 3000);
+        if (state.value !== process.env.NEXT_PUBLIC_SECURITY_CODE) {
+          onError();
+        } else {
+          onConfirm();
+        }
+      }, 1500);
     }
-    console.log("finalizando el efecto");
-  }, [loading]);
-  return (
-    <section className="flex flex-col items-center w-screen h-96 justify-evenly">
-      <h2 className="text-3xl font-bold">Eliminar {name}</h2>
-      <p className="text-2xl ">Por favor, escribe el código de seguridad</p>
-      {error && (
-        <p className="text-xl font-medium text-red-600">
-          error: el código es incorrecto
-        </p>
-      )}
-      {loading && (
-        <p className="text-xl font-medium text-green-600">...cargando</p>
-      )}
-      <div className="flex ">
-        <input
-          placeholder="Código de seguridad"
-          className="p-2 mx-4 text-xl text-center border-2 rounded-md"
-        />
+  }, [state.loading, state.value]);
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <section className="flex flex-col items-center w-screen h-96 justify-evenly">
+        <h3 className="text-3xl font-bold">Eliminar {name}</h3>
+        <p className="text-2xl ">Por favor, escribe el código de seguridad</p>
+        {state.error && !state.loading && (
+          <p className="text-xl font-medium text-red-600">
+            error: el código es incorrecto
+          </p>
+        )}
+        {state.loading && (
+          <p className="text-xl font-medium text-green-600">...cargando</p>
+        )}
+        <div className="flex ">
+          <input
+            placeholder="Código de seguridad"
+            className="p-2 mx-4 text-xl text-center border-2 rounded-md"
+            value={state.value}
+            onChange={(e) => {
+              e.preventDefault();
+              onChangeInput(e.target.value);
+            }}
+          />
+          <button
+            className="p-2 text-xl font-semibold bg-green-200 border-2 border-green-700 rounded-md shadow-lg cursor-pointer text-slate-950 hover:bg-green-800 hover:text-slate-100"
+            onClick={() => onCheck()}
+          >
+            Comprobar
+          </button>
+        </div>
+      </section>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <section className="flex flex-col items-center w-screen h-96 justify-evenly">
+        <h2 className="mt-4 text-3xl font-bold text-center text-stone-800">
+          ¿Seguro que quiere eliminar UseState?
+        </h2>
+        <div className="flex items-center mt-4 justify-evenly">
+          <button
+            className="px-4 py-2 mx-16 text-xl font-medium bg-red-600 border-2 rounded-lg shadow-lg text-stone-100"
+            onClick={() => {
+              onDelete();
+            }}
+          >
+            Si, eliminar
+          </button>
+          <button
+            className="px-4 py-2 mx-16 text-xl font-medium bg-green-600 border-2 rounded-lg shadow-lg text-stone-100"
+            onClick={() => {
+              onReset();
+            }}
+          >
+            No, volver atrás
+          </button>
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="flex flex-col items-center w-screen h-96 justify-evenly">
+        <h2 className="mt-4 text-3xl font-bold text-center text-stone-800">
+          Eliminado con éxito
+        </h2>
         <button
-          className="p-2 text-xl font-semibold bg-green-200 border-2 border-green-700 rounded-md shadow-lg cursor-pointer text-slate-950 hover:bg-green-800 hover:text-slate-100"
-          onClick={() => setLoading(true)}
+          className="px-4 py-2 mx-16 text-xl font-medium bg-green-600 border-2 rounded-lg shadow-lg text-stone-100"
+          onClick={() => {
+            onReset();
+          }}
         >
-          Comprobar
+          Recuperar UseState
         </button>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 };
 
 export default UseState;
